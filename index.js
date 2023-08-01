56b28bb2347bbb0a648a02bd3abe0f2fd2b01258
TomE <tommevans210@gmail.com>
2023-08-01 17:24:07 +0700

    eating pellets

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ class Pellet {
     c.closePath();
   }
 }
-const pellets = []
+const pellets = [];
 const boundaries = [];
 const player = new Player({
   position: {
@@ -404,10 +404,21 @@ function animate() {
     }
   }
 
-pellets.forEach((pellet) =>{
-  pellet.draw()
-})
+  for (let i = pellets.length - 1; 0 < i; i--) {
+    const pellet = pellets[i];
+    pellet.draw();
 
+    if (
+      Math.hypot(
+        pellet.position.x - player.position.x,
+        pellet.position.y - player.position.y
+      ) <
+      pellet.radius + player.radius
+    ) {
+      console.log('touching');
+      pellets.splice(i, 1);
+    }
+  }
 
   boundaries.forEach((boundary) => {
     boundary.draw();
